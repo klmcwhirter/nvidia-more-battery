b70ba79a17d2451f26072ab41bf9bc3802ff44f0
Kevin McWhirter <klmcw@yahoo.com>
2024-03-13 11:29:04 -0700

    Make verbose optional

--- nvidia_more_battery/utils/logging.py
+++ nvidia_more_battery/utils/logging.py
@@ -2,7 +2,7 @@
 import logging
 
 
-def config_logger(*, verbose: bool) -> None:
+def config_logger(*, verbose: bool = False, **_kwargs) -> None:
     level = logging.DEBUG if verbose else logging.INFO
     logging.basicConfig(level=level, format='{asctime} - {module} - {funcName} - {levelname} - {message}', style='{')
     logging.getLogger().setLevel(level=level)
